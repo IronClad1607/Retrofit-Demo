98f0643b8a9665525d869f11f692f688b50d8856
Ishaan Jain <ishaan160799@gmail.com>
2020-06-18 15:01:43 +0530

    option activities created and linked with MainActivity

--- app/src/main/java/com/ironclad/retrofitdemo/activities/MainActivity.kt
+++ app/src/main/java/com/ironclad/retrofitdemo/activities/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.ironclad.retrofitdemo.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ironclad.retrofitdemo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cvUsers.setOnClickListener {
+            val userIntent = Intent(this, UserActivity::class.java)
+            startActivity(userIntent)
+        }
+
+        cvPosts.setOnClickListener {
+            val postIntent = Intent(this, PostActivity::class.java)
+            startActivity(postIntent)
+        }
+
+        cvComments.setOnClickListener {
+            val commentIntent = Intent(this, CommentActivity::class.java)
+            startActivity(commentIntent)
+        }
+
+        cvTodo.setOnClickListener {
+            val todoIntent = Intent(this, TODOActivity::class.java)
+            startActivity(todoIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/ironclad/retrofitdemo/activities/TODOActivity.kt
@@ -0,0 +1,12 @@
+package com.ironclad.retrofitdemo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ironclad.retrofitdemo.R
+
+class TODOActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
