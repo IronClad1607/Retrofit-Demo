fa9c47c83bb5f1877f80df0826883d5cf666edac
Ishaan Jain <ishaan160799@gmail.com>
2020-07-19 13:42:32 +0530

    User Activity:UI Started

--- app/src/main/java/com/ironclad/retrofitdemo/activities/AlbumActivity.kt
+++ app/src/main/java/com/ironclad/retrofitdemo/activities/AlbumActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.ironclad.retrofitdemo.R
 
 class AlbumActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_album)
