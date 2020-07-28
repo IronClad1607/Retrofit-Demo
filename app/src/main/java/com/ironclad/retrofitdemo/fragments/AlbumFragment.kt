0bee454487a1124b8d363b24c288fed14f2a22e4
Ishaan Jain <ishaan160799@gmail.com>
2020-07-28 16:51:04 +0530

    AlbumFragment: UI Started

--- app/src/main/java/com/ironclad/retrofitdemo/fragments/AlbumFragment.kt
+++ app/src/main/java/com/ironclad/retrofitdemo/fragments/AlbumFragment.kt
@@ -8,10 +8,6 @@ import android.view.ViewGroup
 import com.ironclad.retrofitdemo.R
 
 class AlbumFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
